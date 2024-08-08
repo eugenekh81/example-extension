17b3800cf480b3f4e79c0db7c5ab32fe24d3a93d
Katarina Gruener <katarina.gruener@brueder-schlau.de>
2024-08-08 11:32:08 +0200

    new

--- popup.js
+++ popup.js
@@ -1,11 +1,14 @@
-const grabBtn = document.getElementById('grabBtn');
-grabBtn.addEventListener('click', () => {
+const input = document.getElementById('filter');
+const filterBtn = document.getElementById('filterBtn');
+filterBtn.addEventListener('click', () => {
+  const query = input.value;
+  console.log(query, ' this is query');
   // Получить активную вкладку браузера
   chrome.tabs.query({ active: true }, function (tabs) {
     var tab = tabs[0];
     // и если она есть, то выполнить на ней скрипт
     if (tab) {
-      execScript(tab);
+      execScript(tab, query);
     } else {
       alert('There are no active tabs');
     }
@@ -17,54 +20,63 @@ grabBtn.addEventListener('click', () => {
  * вкладки и во всех ее фреймах,
  * @param tab {Tab} Объект вкладки браузера
  */
-function execScript(tab) {
+function execScript(tab, query) {
   // Выполнить функцию на странице указанной вкладки
   // и передать результат ее выполнения в функцию onResult
   chrome.scripting.executeScript(
     {
-      target: { tabId: tab.id, allFrames: true },
-      func: grabImages,
+      target: { tabId: tab.id },
+      func: filter,
     },
     onResult
   );
 }
 
-/**
- * Получает список абсолютных путей всех картинок
- * на удаленной странице
- *
- *  @return Array Массив URL
- */
-function grabImages() {
-  const images = document.querySelectorAll('img');
-  return Array.from(images).map((image) => image.src);
+/* function getFilterValue() {
+  const input = document.querySelector('#filter');
+  console.log(input, '<<<');
+  return input.value
+} */
+
+function filter(query) {
+  document.body.style.color = 'red';
+  const cssInvisibleModifier = "bs-item--invisible";
+  // const filterValue = 'Katarina';
+  //let bar = confirm("Confirm or deny");
+  console.log(query, ' !!!!!!!!!!');
+  let filterMatcher = new RegExp(`.*${query}.*`, "i");
+
+  return filterMatcher;
+
+  /* console.log("Clearing Filter");
+  document.querySelectorAll("." + cssInvisibleModifier).forEach((i) => {
+    i.classList.remove(cssInvisibleModifier);
+  });
+
+  if (query) {
+    console.log("Running Filter");
+    document.querySelectorAll(".fc-event-title").forEach((i) => {
+      if (filterMatcher.test(i.innerText)) {
+      } else {
+        i.parentElement.parentElement.classList.add(cssInvisibleModifier);
+      }
+    });
+  } */
 }
 
-/**
- * Выполняется после того как вызовы grabImages
- * выполнены во всех фреймах удаленной web-страницы.
- * Функция объединяет результаты в строку и копирует
- * список путей к изображениям в буфер обмена
- *
- * @param {[]InjectionResult} frames Массив результатов
- * функции grabImages
- */
-function onResult(frames) {
-  // Если результатов нет
-  if (!frames || !frames.length) {
-    alert('Could not retrieve images from specified page');
-    return;
-  }
-  // Объединить списки URL из каждого фрейма в один массив
-  const imageUrls = frames
-    .map((frame) => frame.result)
-    .reduce((r1, r2) => r1.concat(r2));
-  // Скопировать в буфер обмена полученный массив
-  // объединив его в строку, используя возврат каретки
-  // как разделитель
-  window.navigator.clipboard.writeText(imageUrls.join('\n')).then(() => {
-    // закрыть окно расширения после
-    // завершения
-    window.close();
+function onResult() {
+  console.log("Clearing Filter");
+  document.querySelectorAll("." + cssInvisibleModifier).forEach((i) => {
+    i.classList.remove(cssInvisibleModifier);
   });
+
+  if (query) {
+    console.log("Running Filter");
+    document.querySelectorAll(".fc-event-title").forEach((i) => {
+      if (filterMatcher.test(i.innerText)) {
+      } else {
+        i.parentElement.parentElement.classList.add(cssInvisibleModifier);
+      }
+    });
+  }
 }
