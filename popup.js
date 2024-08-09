07bf83fd467a6f235258d7d0216dc6c106106693
Katarina Gruener <katarina.gruener@brueder-schlau.de>
2024-08-09 08:56:02 +0200

    the element is selected but the rest are not deleted

--- popup.js
+++ popup.js
@@ -1,12 +1,10 @@
 const input = document.getElementById('filter');
 const filterBtn = document.getElementById('filterBtn');
+
 filterBtn.addEventListener('click', () => {
   const query = input.value;
-  console.log(query, ' this is query');
-  // Получить активную вкладку браузера
-  chrome.tabs.query({ active: true }, function (tabs) {
-    var tab = tabs[0];
-    // и если она есть, то выполнить на ней скрипт
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    let tab = tabs[0];
     if (tab) {
       execScript(tab, query);
     } else {
@@ -15,67 +13,31 @@ filterBtn.addEventListener('click', () => {
   });
 });
 
-/**
- * Выполняет функцию grabImages() на веб-странице указанной
- * вкладки и во всех ее фреймах,
- * @param tab {Tab} Объект вкладки браузера
- */
+
 function execScript(tab, query) {
-  // Выполнить функцию на странице указанной вкладки
-  // и передать результат ее выполнения в функцию onResult
   chrome.scripting.executeScript(
     {
       target: { tabId: tab.id },
       func: filter,
-    },
-    onResult
+      args: [query],
+    }
   );
 }
 
-/* function getFilterValue() {
-  const input = document.querySelector('#filter');
-  console.log(input, '<<<');
-  return input.value
-} */
-
 function filter(query) {
-  document.body.style.color = 'red';
   const cssInvisibleModifier = "bs-item--invisible";
-  // const filterValue = 'Katarina';
-  //let bar = confirm("Confirm or deny");
-  console.log(query, ' !!!!!!!!!!');
   let filterMatcher = new RegExp(`.*${query}.*`, "i");
 
-  return filterMatcher;
-
-  /* console.log("Clearing Filter");
   document.querySelectorAll("." + cssInvisibleModifier).forEach((i) => {
     i.classList.remove(cssInvisibleModifier);
   });
 
   if (query) {
-    console.log("Running Filter");
     document.querySelectorAll(".fc-event-title").forEach((i) => {
-      if (filterMatcher.test(i.innerText)) {
-      } else {
+      if (!filterMatcher.test(i.innerText)) {
         i.parentElement.parentElement.classList.add(cssInvisibleModifier);
-      }
-    });
-  } */
-}
-
-function onResult() {
-  console.log("Clearing Filter");
-  document.querySelectorAll("." + cssInvisibleModifier).forEach((i) => {
-    i.classList.remove(cssInvisibleModifier);
-  });
-
-  if (query) {
-    console.log("Running Filter");
-    document.querySelectorAll(".fc-event-title").forEach((i) => {
-      if (filterMatcher.test(i.innerText)) {
       } else {
-        i.parentElement.parentElement.classList.add(cssInvisibleModifier);
+        i.style.color = 'red';
       }
     });
   }
