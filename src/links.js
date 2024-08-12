d7588f5cbfe90fdd9017739fea9abdf2f2bc5d62
Katarina Gruener <katarina.gruener@brueder-schlau.de>
2024-08-12 15:42:13 +0200

    new link

--- src/links.js
+++ src/links.js
@@ -5,30 +5,37 @@ export function createLists() {
 
   const listsData = [
     {
+      title: 'Arbeitszeit',
+      links: [
+        { id: 'timeTracking', text: 'Zeiterfassung', url: 'https://vhbrup01ai01.rot.sap.brueder-schlau.de:44300/sap/bc/webdynpro/sap/zess_clockinout#' },
+        { id: 'entry', text: 'Einstieg', url: 'https://vhbrup01ai02.rot.sap.brueder-schlau.de:44300/nwbc/' }
+      ]
+    },
+    {
       title: 'Dev',
       links: [
-        { id: 'hammerDevLink', text: 'Hammer Dev', url: 'https://local.hammer-zuhause.de:9002/schlaub2cstorefront/' },
-        { id: 'schlauDevLink', text: 'Schlau Dev', url: 'https://local.hammer-zuhause.de:9002/schlaustorefront/' },
-        { id: 'backofficeDev', text: 'Backoffice Dev', url: 'https://local.hammer-zuhause.de:9002/backoffice' }
+        { id: 'hammerDevLink', text: 'Hammer', url: 'https://local.hammer-zuhause.de:9002/schlaub2cstorefront/' },
+        { id: 'schlauDevLink', text: 'Schlau', url: 'https://local.hammer-zuhause.de:9002/schlaustorefront/' },
+        { id: 'backofficeDev', text: 'Backoffice', url: 'https://local.hammer-zuhause.de:9002/backoffice' }
       ]
     },
     {
       title: 'Stage',
       links: [
-        { id: 'hammerStageLink', text: 'Hammer Stage', url: 'https://www.stage.hammer-zuhause.de/' },
-        { id: 'schlauStageLink', text: 'Schlau Stage', url: 'https://www.stage.schlau-grosshandel.de/' },
-        { id: 'backofficeStage', text: 'Backoffice Stage', url: 'https://hybris-cockpits.stage.brueder-schlau.de/backoffice/login.zul' }
+        { id: 'hammerStageLink', text: 'Hammer', url: 'https://www.stage.hammer-zuhause.de/' },
+        { id: 'schlauStageLink', text: 'Schlau', url: 'https://www.stage.schlau-grosshandel.de/' },
+        { id: 'backofficeStage', text: 'Backoffice', url: 'https://hybris-cockpits.stage.brueder-schlau.de/backoffice/login.zul' }
       ]
     },
     {
       title: 'Prod',
       links: [
-        { id: 'hammerProdLink', text: 'Hammer Prod', url: 'https://www.hammer-zuhause.de/' },
-        { id: 'schlauProdLink', text: 'Schlau Prod', url: 'https://www.schlau-grosshandel.de/' }
+        { id: 'hammerProdLink', text: 'Hammer', url: 'https://www.hammer-zuhause.de/' },
+        { id: 'schlauProdLink', text: 'Schlau', url: 'https://www.schlau-grosshandel.de/' }
       ]
     },
     {
-      title: 'Other',
+      title: 'Sonst',
       links: [
         { id: 'ServiceNamesLink', text: 'Servicenamen', url: 'https://confluence.brueder-schlau.de/display/EC/Server-+und+Servicenamen' },
         { id: 'cheatSheet', text: 'eCommerce CheatSheet', url: 'https://confluence.brueder-schlau.de/display/EC/eCommerce+CheatSheet' }
