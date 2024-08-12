d9ffed9da434ac9e2ab0235bb6549b41d0639c45
Katarina Gruener <katarina.gruener@brueder-schlau.de>
2024-08-12 09:02:04 +0200

    servicenames css

--- background.js
+++ background.js
@@ -15,13 +15,13 @@ calendarLink.addEventListener('click', (e) => {
 });
 
 const linksData = [
+  { id: 'ServiceNamesLink', text: 'Servicenamen', url: 'https://confluence.brueder-schlau.de/display/EC/Server-+und+Servicenamen' },
   { id: 'hammerProdLink', text: 'Hammer Prod', url: 'https://www.hammer-zuhause.de/' },
   { id: 'hammerStageLink', text: 'Hammer Stage', url: 'https://www.stage.hammer-zuhause.de/' },
   { id: 'hammerDevLink', text: 'Hammer Dev', url: 'https://local.hammer-zuhause.de:9002/schlaub2cstorefront/' },
   { id: 'schlauProdLink', text: 'Schlau Prod', url: 'https://www.schlau-grosshandel.de/' },
   { id: 'schlauStageLink', text: 'Schlau Stage', url: 'https://www.stage.schlau-grosshandel.de/' },
   { id: 'schlauDevLink', text: 'Schlau Dev', url: 'https://local.hammer-zuhause.de:9002/schlaustorefront/' },
-  { id: 'schlauProdLink', text: 'Schlau Prod', url: 'https://www.schlau-grosshandel.de/' },
   { id: 'backofficeStage', text: 'Backoffice Stage', url: 'https://hybris-cockpits.stage.brueder-schlau.de/backoffice/login.zul' },
   { id: 'backofficeDev', text: 'Backoffice Dev', url: 'https://local.hammer-zuhause.de:9002/backoffice' },
   { id: 'cheatSheet', text: 'eCommerce CheatSheet', url: 'https://confluence.brueder-schlau.de/display/EC/eCommerce+CheatSheet' }
